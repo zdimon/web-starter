336ff230cfe2ed99d74a48900dc7430d45abc210
Your Name <you@example.com>
2020-09-09 12:11:04 +0300

    php

---
+++ docker-php-mysql-nginx/web/index.php
@@ -0,0 +1,17 @@
+<?php
+phpinfo();
+$servername = "db";
+$username = "root";
+$password = "1q2w3e";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  echo "Not Connect";
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
+?>
